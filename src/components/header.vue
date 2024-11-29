a75454d468e48b591e0c3cac9e639c0344e11402
Gabriel-P-Freitas <f.pessoa@escolar.ifrn.edu.br>
2024-11-29 19:54:11 -0300

    iniciando vue

---
+++ src/components/header.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <header class="d-flex justify-content-between align-items-center container mx-auto">
+    <img src="../assets/logo.svg" alt="logo-ifzn-sem-fronteiras">
+
+    <nav>
+      <a href="">Minhas inscrições</a>
+      <a class="px-3" href="">Minhas inscrições</a>
+    </nav>
+
+
+  </header>
+</template>
+
+<style scoped>
+</style>
